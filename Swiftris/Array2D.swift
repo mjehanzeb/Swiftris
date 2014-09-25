0cc7f959aec0dac1ff41ca59a289e27e54862d3f
Muhammad Jehanzeb <mj@kicknturf.com>
2014-09-25 01:02:11 -0400

    updates

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,29 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by KT on 2014-09-25.
+//  Copyright (c) 2014 MJ. All rights reserved.
+//
+
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        array = Array<T?>(count:rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
